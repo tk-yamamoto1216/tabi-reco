ed7314016c58fc9e54def6f40b1000b42ba64ed5
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2021-10-29 22:02:10 +0900

    stylelint を再設定

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-   "extends": [
-     "stylelint-config-recommended-scss",
-     "stylelint-config-recess-order",
-     "stylelint-rscss/config",
-     "stylelint-config-prettier"
-   ],
-   "plugins": ["stylelint-scss"],
-   "rules": {
-     "max-nesting-depth": [
-       1,
-       {
-        "ignore": ["pseudo-classes"],
-        "ignoreAtRules": ["include"]
-       }
-     ]
-   }
- }
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier'
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
