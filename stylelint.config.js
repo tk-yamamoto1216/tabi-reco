8916d421c30b052bae5f080dbdbb7bae850310f3
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2021-10-29 09:49:59 +0900

    stylelint導入

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
-  ],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
-}
+   "extends": [
+     "stylelint-config-recommended-scss",
+     "stylelint-config-recess-order",
+     "stylelint-rscss/config",
+     "stylelint-config-prettier"
+   ],
+   "plugins": ["stylelint-scss"],
+   "rules": {
+     "max-nesting-depth": [
+       1,
+       {
+        "ignore": ["pseudo-classes"],
+        "ignoreAtRules": ["include"]
+       }
+     ]
+   }
+ }
