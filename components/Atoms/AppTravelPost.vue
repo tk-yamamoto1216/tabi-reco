981cb0369e9b57d61ab022be7a72fa22824dde06
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2021-10-30 13:44:18 +0900

    トップページをベタ書き表示

---
+++ components/Atoms/AppTravelPost.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-travel-post">
+    <img
+      src="https://pix10.agoda.net/geo/country/100/3_100_canada_02.jpg?s=1920x"
+      class="thumbnail"
+    />
+    <div class="texts">
+      <p class="title">めっちゃ綺麗でした！</p>
+      <p class="place">カナダ</p>
+      <p class="date">2017/10/20</p>
+      <p class="rate">⭐️ 5.0</p>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="index-page">
     <div class="list">
-      {{ test }}
+      <AppTravelPost v-for="n of 8" :key="n" />
     </div>
+    <div class="map">Map is coming soon...</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api';
+import AppTravelPost from '@/components/Atoms/AppTravelPost.vue';
 
 export default defineComponent({
+  components: {
+    AppTravelPost,
+  },
   setup() {
     const test = ref('旅がしたいか、、');
     return {
