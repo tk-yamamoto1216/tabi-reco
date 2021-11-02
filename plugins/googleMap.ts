f3f59b99031d575e7f67e8725593f8ce43b2077d
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2021-11-02 09:59:40 +0900

    google map api 導入

---
+++ plugins/googleMap.ts
@@ -0,0 +1,18 @@
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: process.env.GOOGLE_MAP_API_KEY || '',
+  version: 'weekly',
+  libraries: ['places'],
+});
+
+const mapOptions = {
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+  zoom: 4,
+};
+
+console.log(loader);
+console.log(mapOptions);
