d0f6ee794dd72d1292875dfdc37bfc79cadc9109
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2021-11-07 11:40:24 +0900

    google map のプラグイン一旦削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: ['@/assets/css/app.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/mock.ts', '~/plugins/googleMap.ts'],
+  plugins: ['~/plugins/mock.ts', '~/plugins/axios.ts'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
