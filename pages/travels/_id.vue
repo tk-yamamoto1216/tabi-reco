830ba9374744aca9c7ecbb0acbddbbb80df9b4db
Takenari Yamamoto <takenari_yamamoto1216@arsaga.jp>
2021-11-09 23:14:43 +0900

    詳細ページ修正成

--- pages/travels/_id.vue
+++ pages/travels/_id.vue
@@ -2,16 +2,44 @@
   <div class="travel-detail">
     <NuxtChild />
     <template v-if="!$route.path.match(/edit/)">
-      <h1 class="title">詳細ページです</h1>
-      <div class="content">
-        <p class="title">{{ travel.title }}</p>
+      <p class="title">{{ travel.title }}</p>
+      <div class="header">
+        <p class="rate">星 4.4</p>
+        <p class="comment">コメント112件</p>
         <p class="place">{{ travel.place }}</p>
         <p class="date">{{ travel.date }}</p>
         <p class="rate">⭐️ {{ travel.good }}</p>
+        <nuxt-link :to="'/travels/' + travel.id + '/edit'" class="btn -white"
+          >編集</nuxt-link
+        >
       </div>
-      <nuxt-link :to="'/travels/' + travel.id + '/edit'" class="btn -white"
-        >編集</nuxt-link
-      >
+      <div class="images">
+        <img
+          class="image1"
+          src="@/assets/images/test/500004814.jpeg"
+          alt="画像1"
+        />
+        <img
+          class="image2"
+          src="@/assets/images/test/65244278-city-11592-162cfe6a30b.jpeg"
+          alt="画像2"
+        />
+        <img
+          class="image3"
+          src="@/assets/images/test/canada_img09.jpeg"
+          alt="画像3"
+        />
+        <img
+          class="image4"
+          src="@/assets/images/test//Photos Library.jpeg"
+          alt="画像4"
+        />
+        <img
+          class="image5"
+          src="@/assets/images/test/ダウンロード.jpeg"
+          alt="画像5"
+        />
+      </div>
     </template>
   </div>
 </template>
